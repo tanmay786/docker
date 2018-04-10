cb67f488e2fcefd2d881488a9e01be20e50149a6
tanmay786 <37614475+tanmay786@users.noreply.github.com>
2018-04-10 12:48:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello Chahat! How are you\n");
 });
 
 // listen on localhost:8000
 server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Server listening at http://35.231.183.24:8000/");
